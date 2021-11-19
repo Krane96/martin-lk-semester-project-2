98c0f902ad75591672e7c5c2d4997a4c996dc983
Krane96 <69842760+Krane96@users.noreply.github.com>
2021-11-19 02:21:42 +0100

    test

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let navigation = document.querySelector('.navigation');
+
+document.querySelector('.menu-icon').onclick = () =>{
+    console.log(event);
+    navigation.classList.toggle('active')
+}
